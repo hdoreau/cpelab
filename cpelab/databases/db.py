ce7de1f202b4e643be164e8040ab30a47fdefac4
Henri Doreau <henri.doreau@gmail.com>
2011-06-24 11:02:10 +0200

    make use of the new db item representation

--- cpelab/databases/db.py
+++ cpelab/databases/db.py
@@ -55,17 +55,17 @@ class Database:
         print '%s:' % self.str_id
         print '\t%d entries loaded' % len(self.entries)
 
-        fp_metacount = {}
+        vendors = set()
         for cpe in self.entries:
-            fp_metacount[cpe.vendor] = True
+            vendors.add(cpe.fields['vendor'])
 
-        print '\t%d vendors' % len(fp_metacount.keys())
+        print '\t%d vendors' % len(vendors)
 
     def lookup(self, pattern):
         """look for a given pattern within the loaded entries"""
         res = []
         for entry in self.entries:
-            for element in entry.get_fields():
+            for element in entry.fields.itervalues():
                 if re.search(pattern, element) is not None:
                     res.append(entry)
                     break
@@ -99,12 +99,7 @@ class Database:
 
 class DBEntry:
     """Represent a single database item"""
-    def __init__(self, title, vendor):
+    def __init__(self):
         """instanciate a new item"""
-        self.title = title
-        self.vendor = vendor
-
-    def get_fields(self):
-        """return the values composing this item"""
-        return [self.title, self.vendor]
+        self.fields = {}
 
